ea44020a3cdaf60f4d1c0cbd51fa90bc22909df0
ROP <photocromatic@gmail.com>
2024-02-24 16:54:52 -0600

    Labs

--- S03/Chall-01-functions.py
+++ S03/Chall-01-functions.py
@@ -1,12 +1,12 @@
 # CHALLENGE: FUNCTIONS
 # crear una función que devuelva el cubo de una lista de números
-nums = [ 2, 3, 5, 7, 6, 9 ]
-nums2 = [ 2, 1, 4, 12, 7 ]
+numsOne = [ 2, 3, 5, 7, 6, 9 ]
+numsTwo = [ 2, 1, 4, 12, 7 ]
 def cubes( n ):
     cubesArray = []
     for x in n:
         cubesArray.append( x * x * x )
     return cubesArray
 
-print(cubes( nums ))
-print(cubes( nums2 ))
+print(cubes( numsOne ))
+print(cubes( numsTwo ))
