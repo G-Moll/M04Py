68e73bac46fbfd3b06acc046d371249677b4eeec
ROP <photocromatic@gmail.com>
2024-02-24 16:41:56 -0600

    Labs

--- S03/Chall-01-functions.py
+++ S03/Chall-01-functions.py
@@ -1,1 +1,10 @@
 # CHALLENGE: FUNCTIONS
+# crear una función que devuelva el cubo de una lista de números
+nums = [ 2,5,7, 6, 9 ]
+def cubes( n ):
+    cubesArray = []
+    for x in n:
+        cubesArray.append( x * x * x )
+    return cubesArray
+
+print(cubes( nums ))
