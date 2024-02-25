26df7dd7bdbc47bc60e468035aa583da4d4772ba
ROP <photocromatic@gmail.com>
2024-02-24 18:32:14 -0600

    Labs

---
+++ S03/Chall-01.2-filter.py
@@ -0,0 +1,22 @@
+# CHALLENGE: FILTER
+# De una lista de diccionarios, filtrar las personas que sean de CDMX y mayores de Edad
+
+people = [ { "name": "Juan", "age": 18, "city": "CDMX" }, { "name": "Alberto", "age": 16, "city": "CDMX" }, { "name": "Peter", "age": 28, "city": "OAX" }, { "name": "Alejandra", "age": 26, "city": "CDMX" }, { "name": "Mariana", "age": 28, "city": "PUE" } ]
+
+def filterPeople( person ):
+    if person[ "age" ] >= 18 and person[ "city" ] == "CDMX":
+        return True
+    else:
+        return False
+
+filteredPeople = filter( filterPeople, people )
+listPeople = list( filteredPeople )
+print( listPeople )
+
+# Map message
+def message( p ):
+    return p[ 'name' ] + " puede votar"
+
+messages = map( message, listPeople )
+print( list( messages  ) )
+# hola :)
