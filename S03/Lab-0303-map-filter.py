3667d66c54fdff146eb4cc32e3c2e0c027256b26
ROP <photocromatic@gmail.com>
2024-03-02 15:55:02 -0600

    Labs

--- S03/Lab-0303-map-filter.py
+++ S03/Lab-0303-map-filter.py
@@ -23,5 +23,5 @@ print( list( filteredTwos ) )
 # Reduce: APLICA UNA FUNCIÓN A CADA ÍNDICE DE UN ARREGLO, PARA GENERAR UN RESULTADO ÚNICO
 import functools
 
-print( "Sum All Array Numbers:", functools.reduce( lambda a, b: a + b, numsTwo ) )
+print( "Sum All Array Numbers:", git functools.reduce( lambda a, b: a + b, numsTwo ) )
 print( "Multiply All Array Numbers:", functools.reduce( lambda x, y: x * y, [ 1, 2, 3, 4 ] ) )
--- S04/Lab-0401-hof.py
+++ S04/Lab-0401-hof.py
@@ -1,1 +1,15 @@
-# OOP
+# HOF High Order Functions
+def outer( fn ):
+    fn()
+def hello():
+    print( "Hello "*3 )
+def bye():
+    print( "Bye "*3 )
+outer( hello )
+outer( bye )
+
+def outerBis( arr ):
+    arr[ 0 ]()
+    arr[ 1 ]()
+outerBis( [ hello, bye ] )
+
