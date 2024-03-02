cbf8164db4a1a389ce15c8321ed3a1669958535d
ROP <photocromatic@gmail.com>
2024-03-02 17:46:26 -0600

    Labs

--- S04/CHALL-02-oop.py
+++ S04/CHALL-02-oop.py
@@ -1,1 +1,3 @@
 # CHALLENGE: OOP
+# Crear clases que representen un Curso y a alumnos
+# Tengan propiedades y metodos
--- S04/Lab-0402-oop.py
+++ S04/Lab-0402-oop.py
@@ -1,28 +1,35 @@
 # OOP Paradigma
 class Student():
-    name: str
+    # public, private, protected
+    __name: str
     age: int
     single: bool
+    balance: int
+
+    @property
+    def name( self ):
+        return self.__name
 
     # def __init__( self, n = "a", a = 10, s = False ):
     #     pass
 
     def __init__( self, n, a, s ):
-        self.name = n
+        self.__name = n
         self.age = a
         self.single = s
 
     def hello( self ):
-        print( "Hello", self.name )
+        print( "Hello", self.__name )
 
 s1 = Student( "Bruno", 33, True )
+s1.name = "Bruno Mars"
 print( s1, s1.name, s1.age, s1.single )
-s1.hello()
-s2 = Student( "Angelica", 17, True )
-print( s2, s2.name, s2.age, s2.single )
-s2.hello()
-# # { name : "Bruno"
-# age : 33
+# s1.hello()
+# s2 = Student( "Angelica", 17, True )
+# print( s2, s2.name, s2.age, s2.single )
+# s2.hello()
+# # # { name : "Bruno"
+# # age : 33
 # single : True}
 
 # { name = "Angelica"
